f27f7a3525dd7679f8533679b3edeb26d2f38440
saurav <sauravpd58@gmail.com>
2018-09-04 01:01:27 +0530

    added javadoc

--- src/main/java/date/DayOfWeekExample1.java
+++ src/main/java/date/DayOfWeekExample1.java
@@ -2,6 +2,11 @@ package date;
 
 import java.time.DayOfWeek;
 
+/**
+ * DayOfWeek ENUM Example
+ * @author saurav
+ *
+ */
 public class DayOfWeekExample1 
 {
 	public static void main(String[] args) 
