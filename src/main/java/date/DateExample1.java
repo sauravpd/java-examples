9c3ebf414b9934b915ba164044d53bc637ecbf1e
saurav <sauravpd58@gmail.com>
2018-09-04 00:55:53 +0530

    added DateExample1 class

---
+++ src/main/java/date/DateExample1.java
@@ -0,0 +1,14 @@
+package date;
+
+import java.util.Date;
+
+public class DateExample1 
+{
+	public static void main(String[] args) 
+	{
+		Date date=new Date();
+		System.out.println(date);
+		
+		System.out.println(new Date(System.currentTimeMillis()));
+	}
+}
