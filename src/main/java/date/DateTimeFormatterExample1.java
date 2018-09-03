19830fa566b2c9f0faf144529b0f398a7415b955
saurav <sauravpd58@gmail.com>
2018-09-04 00:59:37 +0530

    added DateTimeFormatterExample1 class

---
+++ src/main/java/date/DateTimeFormatterExample1.java
@@ -0,0 +1,13 @@
+package date;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatterExample1 
+{
+	public static void main(String[] args) 
+	{
+		DateTimeFormatter format=DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		System.out.println(format.format(LocalDateTime.now()));
+	}
+}
