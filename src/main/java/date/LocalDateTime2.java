c401c8e3a73bf5c683ef09983d62f119f4923a16
saurav <sauravpd58@gmail.com>
2018-09-07 00:12:39 +0530

    added LocalDateTime class

---
+++ src/main/java/date/LocalDateTime2.java
@@ -0,0 +1,17 @@
+package date;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+
+public class LocalDateTime2 
+{
+	public static void main(String[] args) 
+	{
+		LocalDateTime dateTime=LocalDateTime.now();
+		System.out.println(dateTime.get(ChronoField.DAY_OF_MONTH));
+		System.out.println(dateTime.get(ChronoField.DAY_OF_WEEK));
+		System.out.println(dateTime.get(ChronoField.DAY_OF_YEAR));
+		System.out.println(dateTime.get(ChronoField.HOUR_OF_DAY));
+		System.out.println(dateTime.get(ChronoField.MINUTE_OF_DAY));
+	}
+}
