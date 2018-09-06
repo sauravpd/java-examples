98cee0fa14942da1ec1f0d1ce05e5b07c4016fda
saurav <sauravpd58@gmail.com>
2018-09-07 00:15:59 +0530

    added MonthDayExample1

---
+++ src/main/java/date/MonthDayExample1.java
@@ -0,0 +1,17 @@
+package date;
+
+import java.time.LocalDate;
+import java.time.MonthDay;
+
+public class MonthDayExample1 
+{
+	public static void main(String[] args) 
+	{
+		MonthDay month = MonthDay.now();  
+		LocalDate date = month.atYear(2018);  
+		System.out.println(month.getDayOfMonth());
+		System.out.println(month.getMonth());
+		System.out.println(month.getMonthValue());
+		System.out.println(date); 
+	}
+}
