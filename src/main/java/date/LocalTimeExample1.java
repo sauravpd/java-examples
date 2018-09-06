f8f7ffb95fc1f3a0ce9efa2ef0b26ec222ee9096
saurav <sauravpd58@gmail.com>
2018-09-07 00:14:22 +0530

    added LocalTimeExample1 class

---
+++ src/main/java/date/LocalTimeExample1.java
@@ -0,0 +1,16 @@
+package date;
+
+import java.time.LocalTime;
+
+public class LocalTimeExample1 
+{
+	public static void main(String[] args) 
+	{
+		LocalTime time=LocalTime.now();
+		// default format of hour-minute-second.
+		System.out.println(time);
+
+		System.out.println(time.minusHours(1));
+		System.out.println(time.plusHours(1));
+	}
+}
