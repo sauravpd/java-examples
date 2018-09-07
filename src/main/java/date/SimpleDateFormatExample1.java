dd9f151b909c0ab2e7de080b6f403cb3168d705b
saurav <sauravpd58@gmail.com>
2018-09-08 00:34:38 +0530

    added SimpleDateFormatExample1 class

---
+++ src/main/java/date/SimpleDateFormatExample1.java
@@ -0,0 +1,22 @@
+package date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatExample1 
+{
+	public static void main(String[] args) throws ParseException 
+	{
+		Date date = new Date();  
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");  
+		
+		// date to string
+		String dateAfterFormat= format.format(date);  
+		System.out.println(dateAfterFormat);  
+		
+		// String to date
+		Date date1=format.parse("2018/09/15");
+		System.out.println(date1);
+	}
+}
