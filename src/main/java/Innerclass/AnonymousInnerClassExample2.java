df20cbd15a5e67c39286f8f8686b51feea732442
saurav <sauravpd58@gmail.com>
2018-09-10 01:30:17 +0530

    added javadoc

--- src/main/java/Innerclass/AnonymousInnerClassExample2.java
+++ src/main/java/Innerclass/AnonymousInnerClassExample2.java
@@ -1,10 +1,20 @@
 package Innerclass;
 
+/**
+ * Interface car
+ * @author saurav
+ *
+ */
 interface Car
 {
 	void speed();
 }
 
+/**
+ * AnonymousInner Class Example
+ * @author saurav
+ *
+ */
 public class AnonymousInnerClassExample2 
 {
 	private static int speed=100;
