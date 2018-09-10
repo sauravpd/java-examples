9f9adea31a3677d362f7a6499d32f9fc907a8664
saurav <sauravpd58@gmail.com>
2018-09-11 00:57:59 +0530

    added MemberInnerClassExample

---
+++ src/main/java/Innerclass/MemberInnerClassExample.java
@@ -0,0 +1,19 @@
+package Innerclass;
+
+public class MemberInnerClassExample 
+{
+	private int a=10;
+	class MemberInnerClass
+	{
+		private void print()
+		{
+			System.out.println("Value of a : "+a);
+		}
+	}
+	public static void main(String[] args) 
+	{
+		MemberInnerClassExample outerObj=new MemberInnerClassExample();
+		MemberInnerClass innerObj=outerObj.new MemberInnerClass();
+		innerObj.print();
+	}
+}
