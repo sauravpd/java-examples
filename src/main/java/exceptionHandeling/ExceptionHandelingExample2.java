cbba391a28eec3ccdc1a171450b24ae7a14bfe16
saurav <sauravpd58@gmail.com>
2018-09-15 19:37:22 +0530

    added javadoc

--- src/main/java/exceptionHandeling/ExceptionHandelingExample2.java
+++ src/main/java/exceptionHandeling/ExceptionHandelingExample2.java
@@ -1,5 +1,10 @@
 package exceptionHandeling;
 
+/**
+ * Exception Handeling Example
+ * @author saurav
+ *
+ */
 public class ExceptionHandelingExample2 
 {
 	public static void main(String[] args) 
