552c6875b5c4f71f3ce593082f6e2421a376690e
saurav <sauravpd58@gmail.com>
2018-09-15 19:39:54 +0530

    added ExceptionHandelingExample4

---
+++ src/main/java/exceptionHandeling/ExceptionHandelingExample4.java
@@ -0,0 +1,33 @@
+package exceptionHandeling;
+
+public class ExceptionHandelingExample4 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			int grade=34;
+			if(grade>=80)
+			{
+				System.out.print("A");
+			}
+			else if(grade>=60 && grade<80)
+			{
+				System.out.print("B");
+			} 
+			else if(grade>=40 && grade<60)
+			{
+				System.out.print("C");
+			}
+			else
+			{
+				System.out.println("D");
+				throw new ArithmeticException("Fail");
+			}
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
