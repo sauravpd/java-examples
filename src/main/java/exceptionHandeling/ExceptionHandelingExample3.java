dfe39bf85d3acd3f3d5b6340ac9782451ceee0c0
saurav <sauravpd58@gmail.com>
2018-09-15 19:39:23 +0530

    added ExceptionHandelingExample3

---
+++ src/main/java/exceptionHandeling/ExceptionHandelingExample3.java
@@ -0,0 +1,37 @@
+package exceptionHandeling;
+
+public class ExceptionHandelingExample3 
+{
+	@SuppressWarnings("null")
+	public static void main(String[] args) 
+	{
+		try
+		{
+			String str=null;
+			if(str.isEmpty())
+			{
+				System.out.println("String is empty");
+			}
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("ArithmeticException found "+e.getMessage());
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("NumberFormatException found "+e.getMessage());
+		}
+		catch(IndexOutOfBoundsException e)
+		{
+			System.out.println("IndexOutOfBoundsException found "+e.getMessage());
+		}
+		catch(NullPointerException e)
+		{
+			System.out.println("NullPointerException found "+e.getMessage());
+		}
+		finally 
+		{
+			System.out.println("finally block executed");
+		}
+	}
+}
