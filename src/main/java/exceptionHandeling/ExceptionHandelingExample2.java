5d50d669cf54b0513006d749bf1611fd2ac4561e
saurav <sauravpd58@gmail.com>
2018-09-15 19:36:54 +0530

    added ExceptionHandelingExample2
    Signed-off-by: saurav <sauravpd58@gmail.com>

---
+++ src/main/java/exceptionHandeling/ExceptionHandelingExample2.java
@@ -0,0 +1,32 @@
+package exceptionHandeling;
+
+public class ExceptionHandelingExample2 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			String str=null;
+			if(str.isEmpty())
+			{
+				System.out.println("String is empty");
+			}
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("ArithmeticException found "+e.getMessage());
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("NumberFormatException found "+e.getMessage());
+		}
+		catch(IndexOutOfBoundsException e)
+		{
+			System.out.println("IndexOutOfBoundsException found "+e.getMessage());
+		}
+		catch(NullPointerException e)
+		{
+			System.out.println("NullPointerException found "+e.getMessage());
+		}
+	}
+}
