c660f65f7609dc0af51de8e2e6d865257fd4624c
saurav <sauravpd58@gmail.com>
2018-09-19 01:21:24 +0530

    added BufferedOutputStreamExample class

---
+++ src/main/java/filehandling/BufferedOutputStreamExample.java
@@ -0,0 +1,23 @@
+package filehandling;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BufferedOutputStreamExample 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			BufferedOutputStream bos=new BufferedOutputStream(new FileOutputStream(System.getProperty("user.dir")+"//src//main//java//filehandling//b.txt"));
+			bos.write(new String("Welcome to java .... !!!!").getBytes());
+			bos.flush();
+			bos.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
