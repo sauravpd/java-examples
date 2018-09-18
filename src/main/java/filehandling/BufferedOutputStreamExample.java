de2a3f36f7045ad7ec7f2215be343d05ad663914
saurav <sauravpd58@gmail.com>
2018-09-19 01:21:45 +0530

    added javadoc

--- src/main/java/filehandling/BufferedOutputStreamExample.java
+++ src/main/java/filehandling/BufferedOutputStreamExample.java
@@ -4,6 +4,11 @@ import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * BufferedOutputStream Example
+ * @author saurav
+ *
+ */
 public class BufferedOutputStreamExample 
 {
 	public static void main(String[] args) 
