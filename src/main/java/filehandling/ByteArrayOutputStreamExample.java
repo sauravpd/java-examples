c4447a634b4a0da9da8a4e1f13029e6d52143bce
saurav <sauravpd58@gmail.com>
2018-09-20 01:05:26 +0530

    added ByteArrayOutputStreamExample

---
+++ src/main/java/filehandling/ByteArrayOutputStreamExample.java
@@ -0,0 +1,29 @@
+package filehandling;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ByteArrayOutputStreamExample 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			String filepath=System.getProperty("user.dir")+"//src//main//java//filehandling//";
+			FileOutputStream fos1=new FileOutputStream(filepath+"c.txt");
+			FileOutputStream fos2=new FileOutputStream(filepath+"d.txt");
+			ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+			byteArrayOutputStream.write(new String("java file handling example").getBytes());
+			byteArrayOutputStream.writeTo(fos1);
+			byteArrayOutputStream.writeTo(fos2);
+			byteArrayOutputStream.close();
+			fos1.close();
+			fos2.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
