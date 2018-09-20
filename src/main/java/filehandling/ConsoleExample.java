7c9f989f8fca3fa79b868cc079dd28728aa4e540
saurav <sauravpd58@gmail.com>
2018-09-21 01:44:31 +0530

    added ConsoleExample class

---
+++ src/main/java/filehandling/ConsoleExample.java
@@ -0,0 +1,22 @@
+package filehandling;
+
+import java.io.Console;
+
+public class ConsoleExample 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{ 
+			Console c=System.console();    
+			System.out.println("Enter password: ");    
+			char[] ch=c.readPassword();    
+			String pass=String.valueOf(ch);//converting char array into string    
+			System.out.println("Password is: "+pass);     
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
