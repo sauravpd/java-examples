5020c6f62fe2e4a58d3d04e9d53559b95d3d0a00
saurav <sauravpd58@gmail.com>
2018-10-04 23:40:03 +0530

    added CalculationTest class

---
+++ src/main/java/staticImport/CalculationTest.java
@@ -0,0 +1,18 @@
+package staticImport;
+
+import java.util.Scanner;
+import static staticImport.Calulation.add;
+public class CalculationTest 
+{
+	public static void main(String[] args) 
+	{
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter value of a :");
+	int a=scanner.nextInt();
+	System.out.println("Enter value of b :");
+	int b=scanner.nextInt();
+	int sum=add(a, b);
+	System.out.println("Addition is "+sum);
+	scanner.close();
+	}
+}
