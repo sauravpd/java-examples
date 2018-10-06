8e92a40d44c23895bb0231d617fe399d9c46c2ab
saurav <sauravpd58@gmail.com>
2018-10-06 17:06:49 +0530

    added VarargsExample1 class

---
+++ src/main/java/varargs/VarargsExample1.java
@@ -0,0 +1,12 @@
+package varargs;
+
+public class VarargsExample1 
+{
+	public static void main(String... args) 
+	{
+		System.out.println(args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(i + " - " + args[i]);
+		}
+	}
+}
