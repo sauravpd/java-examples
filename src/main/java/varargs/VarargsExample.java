dd3a405f915dc9a78a1adbf6cfad80e988f37a1a
saurav <sauravpd58@gmail.com>
2018-10-06 17:02:56 +0530

    added VarargsExample class

---
+++ src/main/java/varargs/VarargsExample.java
@@ -0,0 +1,19 @@
+package varargs;
+
+public class VarargsExample 
+{
+	public void test1(int... nums) { }
+	public void test2(int start, int... nums) { }
+
+	// COMPILE ERROR
+	/*
+	 * variable argument type int of the method walk3 must be the last parameter
+	 */
+	public void test3(int... nums, int start) { }
+
+	// COMPILE ERROR
+	/*
+	 * Must be once in parameter list
+	 */
+	public void test4(int... start, int... nums) { }
+}
