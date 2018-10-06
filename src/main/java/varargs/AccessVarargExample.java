51e975bcd876442e217925a389a7177491965f8a
saurav <sauravpd58@gmail.com>
2018-10-06 17:02:13 +0530

    added AccessVarargExample class

---
+++ src/main/java/varargs/AccessVarargExample.java
@@ -0,0 +1,16 @@
+package varargs;
+
+public class AccessVarargExample 
+{
+	public void printvalue(int index,int ... numbers)
+	{
+		/*
+		 * Like an array, varargs uses index
+		 */
+		System.out.println(numbers[index]);
+	}
+	public static void main(String[] args) 
+	{
+		new AccessVarargExample().printvalue(1,4,5,6);
+	}
+}
