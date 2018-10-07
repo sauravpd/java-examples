469fc18f8a24062e3f57a1cc1e42fc0be4dbba95
saurav <sauravpd58@gmail.com>
2018-10-08 00:57:25 +0530

    added StringPoolExample class

---
+++ src/main/java/Strings/StringPoolExample.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class StringPoolExample 
+{
+	public static void main(String[] args) 
+	{
+		// Below same strings refers to one object in string pool
+		String str1 = "Hello";
+		String str2 = "Hello";
+		
+		
+		String str3 = new String("Hello");
+
+		System.out.println(str1 == str2);   // true
+		System.out.println(str1 == str3);   // false
+
+	}
+}
