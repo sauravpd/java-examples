2312ec2542573b4c03977f613ea833a98da7ff2a
saurav <sauravpd58@gmail.com>
2018-10-14 02:24:28 +0530

    added Rectangle class

---
+++ src/main/java/Interface/Rectangle.java
@@ -0,0 +1,26 @@
+package Interface;
+
+public class Rectangle 
+{
+	private double length;
+	private double breadth;
+
+	public Rectangle(double length, double breadth) {
+		this.length = length;
+		this.breadth = breadth;
+	}
+
+	public double area() {
+		return length*breadth;
+	}
+
+	public double perimeter() {
+		return 2*(length+breadth);
+	}
+
+	public static void main(String[] args) {
+		Rectangle rectangle = new Rectangle(6.5, 8.5);
+		System.out.println("Area = " + rectangle.area());
+		System.out.println("Perimeter = " + rectangle.perimeter());
+	}
+}
