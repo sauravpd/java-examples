8203c6039e32608d26f33d7a8bc169502a62e4ae
saurav <sauravpd58@gmail.com>
2019-06-04 00:25:29 +0530

    added changes

--- src/main/java/varargs/VarargsExample2.java
+++ src/main/java/varargs/VarargsExample2.java
@@ -9,19 +9,19 @@ public class VarargsExample2
 {
 	// Both can't be compiled together
 	// compiler sees they have same signature
-	static void print(int[] params){ 
-		System.out.println("array");
-	}
-	static void print(int... params){ 
-		System.out.println("varargs");
-	}
+//	static void print(int[] params){ 
+//		System.out.println("array");
+//	}
+//	static void print(int... params){ 
+//		System.out.println("varargs");
+//	}
 
 	public static void main(String[] args) 
 	{
 		// calls only one with Vararg
-		print(1, 2, 3);
+//		print(1, 2, 3);
 
 		// can call any of above two
-		print(new int[] {1, 2, 3});
+//		print(new int[] {1, 2, 3});
 	}
 }
