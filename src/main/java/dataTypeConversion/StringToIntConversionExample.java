452e22bf82a8e806938d0f8003f0a7aaf17cc7fb
saurav <sauravpd58@gmail.com>
2019-06-06 00:25:09 +0530

    added string to int type conversion exmaple

---
+++ src/main/java/dataTypeConversion/StringToIntConversionExample.java
@@ -0,0 +1,11 @@
+package dataTypeConversion;
+
+public class StringToIntConversionExample 
+{
+	public static void main(String[] args) 
+	{
+		String str="500";
+		System.out.println(str+500);
+		System.out.println(Integer.parseInt(str)+500);
+	}
+}
