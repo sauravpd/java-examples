610b84e56333c06ff105e6f483e07cb6f952909a
saurav <sauravpd58@gmail.com>
2019-06-05 15:03:14 +0530

    added boolean type conversion exmaple

---
+++ src/main/java/dataTypeConversion/BooleanToStringExample.java
@@ -0,0 +1,10 @@
+package dataTypeConversion;
+
+public class BooleanToStringExample 
+{
+	public static void main(String[] args) 
+	{
+		boolean booleanVar=true;
+		System.out.println(String.valueOf(booleanVar).length());
+	}
+}
