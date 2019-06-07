b16e97e85dc9ca5b22ed083947220918c2cfa508
saurav <sauravpd58@gmail.com>
2019-06-08 01:21:32 +0530

    added int type conversion exmaple

---
+++ src/main/java/dataTypeConversion/IntToDoubleExample.java
@@ -0,0 +1,12 @@
+package dataTypeConversion;
+
+public class IntToDoubleExample 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		double d=a;
+		System.out.println(a);
+		System.out.println(d);
+	}
+}
