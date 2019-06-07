e3fe87ca9a1ea70cd3d6cd19ad35ba75bd89473f
saurav <sauravpd58@gmail.com>
2019-06-07 22:53:34 +0530

    added int type conversion exmaple

---
+++ src/main/java/dataTypeConversion/IntToCharExample.java
@@ -0,0 +1,12 @@
+package dataTypeConversion;
+
+public class IntToCharExample 
+{
+	public static void main(String[] args) 
+	{
+		int a=65;
+		char charVar=(char) a;
+		System.out.println(a);
+		System.out.println(charVar);
+	}
+}
