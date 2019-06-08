d015a6a195756dbbe70ef9c9dde7fd60625027d7
saurav <sauravpd58@gmail.com>
2019-06-09 00:23:12 +0530

    commit changes

---
+++ src/main/java/dataTypeConversion/DoubleToIntExample.java
@@ -0,0 +1,12 @@
+package dataTypeConversion;
+
+public class DoubleToIntExample 
+{
+	public static void main(String[] args) 
+	{
+		double a=10.20;
+		int b=(int) a;//typecasting
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
