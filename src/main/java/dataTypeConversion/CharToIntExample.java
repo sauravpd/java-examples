e0a3a64a937640567a7f8723cd3868f9551514f6
saurav <sauravpd58@gmail.com>
2019-06-12 00:52:42 +0530

    char to int example

---
+++ src/main/java/dataTypeConversion/CharToIntExample.java
@@ -0,0 +1,12 @@
+package dataTypeConversion;
+
+public class CharToIntExample 
+{
+	public static void main(String[] args) 
+	{
+		char charVar='A';
+		int a=charVar;
+		System.out.println(charVar);
+		System.out.println(a);
+	}
+}
