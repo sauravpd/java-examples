3356185a8ee10d1082900488912c86f530674af1
saurav <sauravpd58@gmail.com>
2019-06-11 10:01:41 +0530

    added string to long type conversion

---
+++ src/main/java/dataTypeConversion/StringToLongConversionExample.java
@@ -0,0 +1,11 @@
+package dataTypeConversion;
+
+public class StringToLongConversionExample 
+{
+	public static void main(String[] args) 
+	{
+		String str="9876543210";
+		long longNum=Long.parseLong(str);
+		System.out.println(longNum);
+	}
+}
