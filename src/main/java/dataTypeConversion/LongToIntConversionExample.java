fbd0b176c9e3bff4780dc02c2eafd33b1f997940
saurav <sauravpd58@gmail.com>
2019-06-13 00:38:41 +0530

    long to int example

---
+++ src/main/java/dataTypeConversion/LongToIntConversionExample.java
@@ -0,0 +1,12 @@
+package dataTypeConversion;
+
+public class LongToIntConversionExample 
+{
+	public static void main(String[] args) 
+	{
+		long a=10000;
+		int b=(int) a;//type casting
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
