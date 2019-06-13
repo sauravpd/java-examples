92b2db9a3166daa5bdf510c65584203f2d8e8ea3
saurav <sauravpd58@gmail.com>
2019-06-14 00:50:27 +0530

    float to string example

---
+++ src/main/java/dataTypeConversion/FloatToStringConversionExample.java
@@ -0,0 +1,11 @@
+package dataTypeConversion;
+
+public class FloatToStringConversionExample 
+{
+	public static void main(String[] args) 
+	{
+		float floatNum=3.14f;
+		System.out.println(String.valueOf(floatNum)+1.12);
+		System.out.println(Float.toString(floatNum));
+	}
+}
