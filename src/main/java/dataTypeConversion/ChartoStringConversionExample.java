cbcb0cd1f2197506ba6bc721bbcefbade8709083
saurav <sauravpd58@gmail.com>
2019-06-18 10:27:05 +0530

    added ChartoStringConversionExample

---
+++ src/main/java/dataTypeConversion/ChartoStringConversionExample.java
@@ -0,0 +1,11 @@
+package dataTypeConversion;
+
+public class ChartoStringConversionExample 
+{
+	public static void main(String[] args) 
+	{
+		char character='A';
+		System.out.println(String.valueOf(character));
+		System.out.println(String.valueOf(character).charAt(0));
+	}
+}
