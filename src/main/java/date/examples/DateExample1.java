2a0020c4174dd87b1098f075ef1e70aa7af99fdf
saurav <sauravpd58@gmail.com>
2019-06-18 10:37:08 +0530

    commit changes

---
+++ src/main/java/date/examples/DateExample1.java
@@ -0,0 +1,12 @@
+package date.examples;
+
+import java.util.Date;
+
+public class DateExample1 
+{
+	public static void main(String[] args) 
+	{
+		Date date=new Date();
+		System.out.println(date);
+	}
+}
