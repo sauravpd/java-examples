744a505f82c97222e8984446b2377dbe15aabfb7
saurav <sauravpd58@gmail.com>
2019-06-19 11:04:51 +0530

    commit changes

---
+++ src/main/java/dataTypeConversion/DoubleToStringConversionExample.java
@@ -0,0 +1,11 @@
+package dataTypeConversion;
+
+public class DoubleToStringConversionExample 
+{
+	public static void main(String[] args) 
+	{
+		double doubleNum=25.5024;
+		System.out.println(String.valueOf(doubleNum));
+		System.out.println(Double.toString(doubleNum));
+	}
+}
