05d8d10ab163958c72d4c2db9a2e97e929ffb053
saurav <sauravpd58@gmail.com>
2019-06-22 20:11:48 +0530

    added date example

---
+++ src/main/java/date/examples/DateExample2.java
@@ -0,0 +1,12 @@
+package date.examples;
+
+import java.util.Date;
+
+public class DateExample2 
+{
+	public static void main(String[] args) 
+	{
+		Date date=new Date(System.currentTimeMillis());
+		System.out.println(date);
+	}
+}
