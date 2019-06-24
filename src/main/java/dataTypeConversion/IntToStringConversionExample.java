d69e5a67d66dfc3f16fa62435a65d75f35cbaf8e
saurav <sauravpd58@gmail.com>
2019-06-25 01:20:22 +0530

    added IntToStringConversionExample class

---
+++ src/main/java/dataTypeConversion/IntToStringConversionExample.java
@@ -0,0 +1,11 @@
+package dataTypeConversion;
+
+public class IntToStringConversionExample 
+{
+	public static void main(String[] args)
+	{
+		int a=500;
+		System.out.println(a+a);
+		System.out.println(String.valueOf(a)+a);
+	}
+}
