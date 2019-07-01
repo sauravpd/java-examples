0590d6bcc81b954acaa26cac7660f9c173c3955a
saurav <sauravpd58@gmail.com>
2019-07-01 10:11:00 +0530

    added StringToDoubleConversionExample class

---
+++ src/main/java/dataTypeConversion/StringToDoubleConversionExample.java
@@ -0,0 +1,10 @@
+package dataTypeConversion;
+
+public class StringToDoubleConversionExample 
+{
+	public static void main(String[] args) 
+	{
+		String doubleStr="25.5012";
+		System.out.println(Double.parseDouble(doubleStr));
+	}
+}
