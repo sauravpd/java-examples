beaca5c629d965be05e120e85d3e4d493ce1795c
saurav <sauravpd58@gmail.com>
2019-07-05 01:07:45 +0530

    added DateExample6

---
+++ src/main/java/date/examples/DateExample6.java
@@ -0,0 +1,12 @@
+package date.examples;
+
+import java.time.LocalDate;
+
+public class DateExample6
+{
+	public static void main(String[] args) 
+	{
+		LocalDate date=LocalDate.now();
+		System.out.println(date);
+	}
+}
