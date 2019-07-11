60a07fccad08c989928ef5094fe828a4d548e4af
saurav <sauravpd58@gmail.com>
2019-07-11 10:14:02 +0530

    LongToStringConversionExample class

---
+++ src/main/java/dataTypeConversion/LongToStringConversionExample.java
@@ -0,0 +1,12 @@
+package dataTypeConversion;
+
+public class LongToStringConversionExample 
+{
+	public static void main(String[] args) 
+	{
+		long longNum=987653210L;
+		System.out.println(longNum+10);
+		System.out.println(String.valueOf(longNum)+10);
+		
+	}
+}
