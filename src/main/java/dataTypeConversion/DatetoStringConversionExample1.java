cdbcee51ca905a7d167f0aedd5edd1098da4bc90
saurav <sauravpd58@gmail.com>
2019-07-17 15:20:30 +0530

    added DatetoStringConversionExample1 class

---
+++ src/main/java/dataTypeConversion/DatetoStringConversionExample1.java
@@ -0,0 +1,15 @@
+package dataTypeConversion;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DatetoStringConversionExample1 
+{
+	public static void main(String[] args) 
+	{
+		Date date=new Date();
+		SimpleDateFormat formatter=new SimpleDateFormat("dd/MM/yyyy");
+		String strDate=formatter.format(date);
+		System.out.println(strDate);
+	}
+}
