d53d2abbdb8041612c3571771ad2b13ba7da0be6
saurav <sauravpd58@gmail.com>
2019-07-20 18:27:29 +0530

    added IntToLongConversionExample class

---
+++ src/main/java/dataTypeConversion/IntToLongConversionExample.java
@@ -0,0 +1,12 @@
+package dataTypeConversion;
+
+public class IntToLongConversionExample 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		long b=a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
