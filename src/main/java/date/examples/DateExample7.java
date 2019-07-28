8e38d580a5e80882f505e985c2ed129b250ddbad
saurav <sauravpd58@gmail.com>
2019-07-28 10:51:52 -0700

    added DateExample7

---
+++ src/main/java/date/examples/DateExample7.java
@@ -0,0 +1,12 @@
+package date.examples;
+
+import java.time.LocalTime;
+
+public class DateExample7 
+{
+	public static void main(String[] args) 
+	{
+		LocalTime time=LocalTime.now();
+		System.out.println(time);
+	}
+}
