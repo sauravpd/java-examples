6109240893a7d02d28ca988a1a62b1a1219ec5fd
saurav <sauravpd58@gmail.com>
2021-01-19 00:33:11 +0530

    commit changes

---
+++ src/main/java/Strings/StringProgram2.java
@@ -0,0 +1,38 @@
+package Strings;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class StringProgram2 {
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String input=sc.nextLine();
+		System.out.println(firstUniqChar(input));
+		sc.close();
+	}
+
+	public static int firstUniqChar(String s) {
+		HashMap<Character,Integer> map=new HashMap<>();
+		for(int i=0;i<s.length();i++){
+			char c=s.charAt(i);
+			if(!map.containsKey(c)){
+				map.put(c,i);
+			}
+			else{
+				map.put(c,-1);
+			}
+		}
+
+		int min=Integer.MAX_VALUE;
+		for(char c:map.keySet()){
+			if(map.get(c)>-1 && map.get(c)<min)
+			{
+				min=map.get(c);
+			}
+		}
+		return min==Integer.MAX_VALUE?-1:min;
+	}
+}
+
+
