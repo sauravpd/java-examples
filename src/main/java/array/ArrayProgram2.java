a350a96397093f04a8df83042eed3564e61d135e
saurav <sauravpd58@gmail.com>
2021-01-25 20:39:47 +0530

    added Array program 2

--- src/main/java/array/ArrayProgram2.java
+++ src/main/java/array/ArrayProgram2.java
@@ -1,5 +1,32 @@
 package array;
 
-public class ArrayProgram2 {
+import java.util.HashSet;
 
+/**
+ * 
+ * find array contains any duplicates.
+ * 
+ * @author saurav
+ *
+ */
+public class ArrayProgram2 {
+	public static void main(String[] args) {
+		int[] inputArray1 = new int[] { 4, 3, 3, 4 };
+		System.out.println(containsDuplicate(inputArray1)); // true
+
+		int[] inputArray2 = new int[] { 1, 2, 3, 4 };
+		System.out.println(containsDuplicate(inputArray2)); // false
+	}
+
+	private static boolean containsDuplicate(int[] inputArray) {
+		HashSet<Integer> set = new HashSet<>();
+		for (int num : inputArray) {
+			if (set.contains(num)) {
+				return true;
+			} else {
+				set.add(num);
+			}
+		}
+		return false;
+	}
 }
