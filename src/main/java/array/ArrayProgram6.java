a588a8e376f496ac09630f26f1df21b61246208d
saurav <sauravpd58@gmail.com>
2021-01-30 00:28:05 +0530

    added array program 6

---
+++ src/main/java/array/ArrayProgram6.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * @author saurav
+ *
+ */
+public class ArrayProgram6 {
+	public static void main(String[] args) {
+		int[] inputArray1 = new int[] {0,9,0,6,20 };
+		System.out.println(Arrays.toString(moveZeroes(inputArray1))); // [9, 6, 20, 0, 0]
+	}
+
+	public static int[] moveZeroes(int[] nums) {
+		int index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				nums[index++] = nums[i];
+			}
+		}
+		for (int i = index; i < nums.length; i++) {
+			nums[i] = 0;
+		}
+		return nums;
+	}
+}
