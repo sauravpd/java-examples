58187b080e9b0a3520c9e1eadac74e000a60edcb
saurav <sauravpd58@gmail.com>
2021-05-12 05:03:56 +0530

    added Math operatio test

---
+++ src/main/java/java8/lambdaExpression/MathOperationtest.java
@@ -0,0 +1,21 @@
+package java8.lambdaExpression;
+
+public class MathOperationtest 
+{
+	public static void main(String[] args)
+	{
+		MathOperation add = (a, b) -> a + b;
+		MathOperation sub = (a, b) -> a - b;
+		MathOperation mult = (a, b) -> a * b;
+		MathOperation div = (a, b) -> a / b;
+		
+		System.out.println(caculate(add));
+		System.out.println(caculate(sub));
+		System.out.println(caculate(div));
+		System.out.println(caculate(mult));
+	}
+	
+	private static int caculate(MathOperation op) {
+		return op.operate(10,10);
+	}
+}
